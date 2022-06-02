0c54ec9abeefa50249b43b9150a039da3d9067d7
chris <chrislanderos05@gmail.com>
2022-06-02 10:29:39 -0700

    added login api

---
+++ client/JS/loginpage.js
@@ -0,0 +1,22 @@
+import { apiService as api} from '../api/api.js';
+
+document.getElementById('loginbuttton').addEventListener('submit', Login)
+
+function addEmployee(event){
+  event.preventDefault();
+
+  let username = document.getElementById("user").value
+  let password = document.getElementById("password").value
+ 
+  
+  api.post('login-user', {
+     username:username, 
+     password:password, 
+  })
+  
+    removeDetails()
+}
+
+  function removeDetails(){
+    document.login_page.reset();
+}
