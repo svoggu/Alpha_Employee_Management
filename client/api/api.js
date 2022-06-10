873a26167d194eded5a0c610e3ea113d17410d3d
Sirisha <svoggu@alphaworks.tech>
2022-06-10 11:42:33 -0700

    fix(client): fix api path

--- client/api/api.js
+++ client/api/api.js
@@ -1,5 +1,5 @@
 export const apiService = {
-  baseUrl : "http://localhost:3002/api/",
+  baseUrl : process.env.HOST + "/api/",
  async get(url) {
     return fetch(this.baseUrl + url).then((res) => res.json());
   },
